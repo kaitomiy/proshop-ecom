e959088788331ae5ddef763a00103914e960de10
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-01 16:18:44 +0900

    add product route

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 
 const connectDB = require('./config/db');
-const products = require('./data/products');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 
@@ -15,15 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 

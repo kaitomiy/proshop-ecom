5c2e22e329c07eaba27173c1a28ad602fe833f58
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-01 14:12:29 +0900

    add colors dependency

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+
 const connectDB = require('./config/db');
 const products = require('./data/products');
 
@@ -27,5 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );

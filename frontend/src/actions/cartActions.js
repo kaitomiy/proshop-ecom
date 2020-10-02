806fa98efea12587506e26a20129455a94ba854c
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-02 10:34:03 +0900

    add addtocart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -4,7 +4,7 @@ import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
 
-  dispatchEvent({
+  dispatch({
     type: CART_ADD_ITEM,
     payload: {
       product: data._id,
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Form, Card } from 'react-bootstrap';
 
-const CartScreen = () => {
+import Messgae from '../components/Message';
+import { addToCart } from '../actions/cartActions';
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>CartScreen</div>;
 };
 

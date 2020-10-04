d5a7267448cdc3b880b28575882614608cc295ef
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-04 11:23:58 +0900

    add update order to paid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require('../controllers/orderController');
 const protect = require('../middleware/authMiddleware');
 
 router.route('/').post(protect, addOrderItems);
 
 router.route('/:id').get(protect, getOrderById);
+
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 module.exports = router;

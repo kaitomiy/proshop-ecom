e939f4dc4de64ef5f399b92f2da9f018b54f57da
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-06 19:54:25 +0900

    add top rated products carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,12 +8,15 @@ const {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } = require('../controllers/productController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 router.route('/:id/reviews').post(protect, createProductReview);
+
+router.get('/top', getTopProducts);
 
 router
   .route('/:id')

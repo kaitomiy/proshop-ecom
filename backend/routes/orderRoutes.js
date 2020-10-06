e20ef019d51e876196684c52ea921beb6bea1de9
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-06 10:01:22 +0900

    add admin list orders screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,10 +6,11 @@ const {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 } = require('../controllers/orderController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 
-router.route('/').post(protect, addOrderItems);
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 
 router.route('/myorders').get(protect, getMyOrders);
 
